e91bd39ed347eb8b7f3a3198a44bea5c252ffe49
anthony2399 <anthonykhoury23991@hotmail.com>
2025-08-28 12:02:48 +0300

    fix: added next config and build error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,51 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+        // Posthog
+        {
+            source: '/analytics/:path*',
+            destination: 'https://eu.posthog.com/:path*',
+        },
+        {
+            source: '/analytics/:path*/',
+            destination: 'https://eu.posthog.com/:path*/',
+        },
+    ]
+},
+async headers() {
+    async function getMyIp() {
+        const x = await fetch('https://api.ipify.org')
+        // const x = await fetch('https://api.my-ip.io/ip')
+        return await x.text()
+    }
+    const ip = await getMyIp()
+    return [
+        {
+            source: '/analytics/:path*',
+            headers: [
+                { key: 'X-Forwarded-Proto', value: 'https' },
+                {
+                    key: 'X-Forwarded-Host',
+                    value: 'https://www.useflytrap.com',
+                },
+                { key: 'X-Forwarded-For', value: ip },
+            ],
+        },
+        {
+            source: '/analytics/:path*/',
+            headers: [
+                { key: 'X-Forwarded-Proto', value: 'https' },
+                {
+                    key: 'X-Forwarded-Host',
+                    value: 'https://www.useflytrap.com',
+                },
+                { key: 'X-Forwarded-For', value: ip },
+            ],
+        },
+    ]
+},
 };
 
 export default nextConfig;
